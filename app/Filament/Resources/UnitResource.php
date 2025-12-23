2ab7dada4702e2a69a1e152f4f314f08e250652e
Jean Escalante <jeancarlos.escalante@hotmail.com>
2025-12-23 15:34:27 -0400

    Add UnitResource and ManageUnits page for managing unit measures; define form and table structure for unit management.

---
+++ app/Filament/Resources/UnitResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UnitResource\Pages;
+use App\Filament\Resources\UnitResource\RelationManagers;
+use App\Models\Unit;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UnitResource extends Resource
+{
+    protected static ?string $model = Unit::class;
+
+    protected static ?string $pluralLabel = 'Unidades de medida';
+
+    protected static ?string $label = 'Unidad de medida';
+
+    protected static ?string $navigationGroup = 'Configuración';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('code')
+                    ->label('Código'),
+                Forms\Components\TextInput::make('name')
+                    ->label('Nombre')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code')
+                    ->label('Código'),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nombre'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageUnits::route('/'),
+        ];
+    }
+}
