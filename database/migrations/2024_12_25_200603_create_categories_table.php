5e93a293bfd9501d3cd2ef9ae05dde036537b4e1
Jean Escalante <jeancarlos.escalante@hotmail.com>
2025-12-29 13:20:40 -0400

    feat: Establish core sales and inventory system with initial database migrations, models, and services, including a project roadmap.

--- database/migrations/2024_12_25_200603_create_categories_table.php
+++ database/migrations/2024_12_25_200603_create_categories_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->bigIncrements('category_id');
             $table->string('category_name');
             $table->string('description')->nullable();
-            $table->timestamps();
         });
     }
 
